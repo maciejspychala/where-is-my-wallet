fbb1f83083c9d885209db33ded3a1fad80b1227c
maciejspychala <mac.spychala@gmail.com>
2016-10-10 16:10:37 +0200

    check for permission

--- app/src/main/java/com/example/maciej/wallet/MainActivity.java
+++ app/src/main/java/com/example/maciej/wallet/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.maciej.wallet;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkForPermission();
     }
 
     public void showFragment(Fragment fragment) {
@@ -24,5 +28,13 @@ public class MainActivity extends AppCompatActivity {
             transaction.addToBackStack(null);
         }
         transaction.commit();
+    }
+
+    private void checkForPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //TODO: display splash
+        } else {
+            //TODO: display menu
+        }
     }
 }
