9e86762701f33fac3455c1a20fec5ed5b90dd160
maciejspychala <mac.spychala@gmail.com>
2016-10-10 21:17:31 +0200

    added MapFragment MVP interfaces

---
+++ app/src/main/java/com/example/maciej/wallet/map/MapFragmentPresenterInterface.java
@@ -0,0 +1,17 @@
+package com.example.maciej.wallet.map;
+
+import android.location.Location;
+
+import com.example.maciej.wallet.base.BasePresenterInterface;
+
+/**
+ * Created by maciej on 10/10/16.
+ */
+public interface MapFragmentPresenterInterface extends BasePresenterInterface {
+
+    void positionChoosed(Location location);
+
+    String formatTitle(String title);
+
+    void positionChoosed();
+}
---
+++ app/src/main/java/com/example/maciej/wallet/map/MapFragmentView.java
@@ -0,0 +1,13 @@
+package com.example.maciej.wallet.map;
+
+import com.example.maciej.wallet.base.BaseView;
+
+/**
+ * Created by maciej on 09/10/16.
+ */
+public interface MapFragmentView extends BaseView {
+
+    void setTitle(String text);
+
+    void close();
+}
