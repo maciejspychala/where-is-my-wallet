edce5bce16e999465506f0546844096ba63ca559
maciejspychala <mac.spychala@gmail.com>
2016-10-10 17:40:35 +0200

    added MenuFragment MVP interfaces

---
+++ app/src/main/java/com/example/maciej/wallet/menu/MenuFragmentPresenterInterface.java
@@ -0,0 +1,10 @@
+package com.example.maciej.wallet.menu;
+
+import com.example.maciej.wallet.base.BasePresenterInterface;
+
+/**
+ * Created by maciej on 10/10/16.
+ */
+public interface MenuFragmentPresenterInterface extends BasePresenterInterface {
+    void itemSelected(String key);
+}
---
+++ app/src/main/java/com/example/maciej/wallet/menu/MenuFragmentView.java
@@ -0,0 +1,10 @@
+package com.example.maciej.wallet.menu;
+
+import com.example.maciej.wallet.base.BaseView;
+
+/**
+ * Created by maciej on 09/10/16.
+ */
+public interface MenuFragmentView extends BaseView {
+    void openMapFragment(String key);
+}
