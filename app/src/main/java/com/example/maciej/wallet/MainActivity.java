d395e35729c5956b429bbd189718e2c7f849e7d9
maciejspychala <mac.spychala@gmail.com>
2016-10-10 15:52:01 +0200

    added showFragment method to MainActivity

--- app/src/main/java/com/example/maciej/wallet/MainActivity.java
+++ app/src/main/java/com/example/maciej/wallet/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.maciej.wallet;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showFragment(Fragment fragment) {
+        showFragment(fragment, false);
+    }
+
+    public void showFragment(Fragment fragment, boolean backstack) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.activity_main_root, fragment);
+        if (backstack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
     }
 }
