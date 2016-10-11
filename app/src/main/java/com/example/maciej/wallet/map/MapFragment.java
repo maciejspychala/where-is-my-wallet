d654fb3444d51d8b3073ce8b6ad3a7cf5bf83542
maciejspychala <mac.spychala@gmail.com>
2016-10-11 12:12:36 +0200

    display map in MapView inside MapFragment

--- app/src/main/java/com/example/maciej/wallet/map/MapFragment.java
+++ app/src/main/java/com/example/maciej/wallet/map/MapFragment.java
@@ -1,21 +1,57 @@
 package com.example.maciej.wallet.map;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.maciej.wallet.R;
 import com.example.maciej.wallet.base.BaseFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import butterknife.BindView;
 
 /**
  * Created by maciej on 11/10/16.
  */
-public class MapFragment extends BaseFragment<MapFragmentPresenterInterface> implements MapFragmentView {
+public class MapFragment extends BaseFragment<MapFragmentPresenterInterface> implements MapFragmentView, OnMapReadyCallback {
 
-    public enum Item {CAR, WALLET};
+    @BindView(R.id.map_view)
+    MapView mapView;
 
     @BindView(R.id.map_title)
     TextView title;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
 
     @Override
     protected MapFragmentPresenterInterface initFragmentPresenter() {
@@ -36,4 +72,11 @@ public class MapFragment extends BaseFragment<MapFragmentPresenterInterface> imp
     public void close() {
         getActivity().getSupportFragmentManager().popBackStack();
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    public enum Item {CAR, WALLET}
 }
